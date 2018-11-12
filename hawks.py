0568769712e6d3fda8a9bb594c1f07429682b26e
Erick Hammersmark <bink@uffda.com>
2018-11-12 08:28:57 -0800

    added mock rgb lib and image printer for local development

--- hawks.py
+++ hawks.py
@@ -2,10 +2,17 @@
 
 import argparse
 import BaseHTTPServer
+import os
 import sys
 import time
-from rgbmatrix import RGBMatrix, RGBMatrixOptions
+
 from PIL import Image, ImageDraw, ImageFont
+
+nodename = os.uname()[1]
+if nodename == 'raspberrypi':
+  from rgbmatrix import RGBMatrix, RGBMatrixOptions
+else:
+  from mock import RGBMatrix, RGBMatrixOptions
 
 global matrix
 
@@ -13,6 +20,7 @@ PRESETS = {
     "dark": {"bgcolor": "black", "innercolor": "blue", "outercolor": "green"},
     "bright": {"bgcolor": "blue", "innercolor": "black", "outercolor": "green"},
     "blue_on_green": {"bgcolor": "green", "innercolor": "blue", "outercolor": "black"},
+    "none": {},
 }
 
 def parse_args():
@@ -30,8 +38,37 @@ def parse_args():
   parser.add_argument("--thickness", type=int, default=1)
   parser.add_argument("--preset", default=None, choices=PRESETS.keys())
   parser.add_argument("--port", type=int, default=1212)
+  parser.add_argument("--debug", action="store_true", default=False)
   return parser.parse_args()
   return args
+
+def text_as_color(text, rgb):
+  '''
+  Return string with text prefixed by ANSI escape
+  code to set the backgorund to the color specified
+  by 'rgb' (a tuple of r, g, b bytes).  The string
+  also include the escape sequence to set the terminal
+  back to its default colors.
+  '''
+  escape_seq = '\033[48;2;{0};{1};{2}m{3}\033[10;m'
+  r, g, b = rgb
+  return escape_seq.format(r, g, b, text)
+
+def print_image(image):
+  #\033[38;2;255;82;197;48;2;155;106;0mHello
+  count = 0
+  print
+  for px in image.getdata():
+    sys.stdout.write(text_as_color('  ', px))
+    count += 1
+    if count % 32 == 0:
+      print
+  print
+
+def set_image(args, matrix, image):
+  if args.debug:
+    print_image(image)
+  matrix.SetImage(image)
 
 def init_matrix(args):
   # Configuration for the matrix
@@ -41,7 +78,8 @@ def init_matrix(args):
   options.parallel = 1
   options.hardware_mapping = 'adafruit-hat'  # If you have an Adafruit HAT: 'adafruit-hat'
   matrix = RGBMatrix(options = options)
-  matrix.SetImage(Image.new("RGB", (args.cols, args.rows), "black"))
+  #matrix.SetImage(Image.new("RGB", (args.cols, args.rows), "black"))
+  set_image(args, matrix, Image.new("RGB", (args.cols, args.rows), "black"))
   return matrix
 
 def draw_text(args):
@@ -64,14 +102,12 @@ def draw_text(args):
 
   draw.text((x, y), args.text, fill=args.innercolor, font=font)
 
-  matrix.SetImage(image)
+  #matrix.SetImage(image)
+  set_image(args, matrix, image)
 
-def main():
-  global matrix
-  args = parse_args()
-  matrix = init_matrix(args)
-  draw_text(args)
+  return image
 
+def run_api_forever(args):
   class HawksRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     def __init__(self, *a, **kw):
       self.args = args
@@ -88,6 +124,8 @@ def main():
       if self.path == "/exit":
         sys.exit(0)
       parts = self.path.split('/')
+      if len(parts) < 3:
+        return self.send(404)
       key = parts[1]
       value = parts[2]
       if hasattr(self.args, key):
@@ -95,12 +133,20 @@ def main():
           value = int(value)
         setattr(self.args, key, value)
         print(args.preset)
-        draw_text(self.args)
+        image = draw_text(self.args)
         return self.send(200)
       self.send(404)
 
   httpd = BaseHTTPServer.HTTPServer(('', args.port), HawksRequestHandler)
   httpd.serve_forever()
+
+def main():
+  global matrix
+  args = parse_args()
+  matrix = init_matrix(args)
+  draw_text(args)
+  run_api_forever(args)
+
 
 if __name__ == '__main__':
   main()
---
+++ mock.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import sys
+
+class RGBMatrix(object):
+  def __init__(self, *args, **kwargs):
+    pass
+  def SetImage(self, image, *args, **kwargs):
+    pass
+
+class RGBMatrixOptions(object):
+  def __init__(self, *args, **kwargs):
+    pass
+
