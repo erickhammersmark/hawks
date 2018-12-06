b7d05ddd7b6405f2f991c8c8084ad0aca64b4c62
Erick Hammersmark <bink@uffda.com>
2018-12-05 17:33:11 -0800

    json POST support for /api/set

--- hawks_api.py
+++ hawks_api.py
@@ -21,6 +21,28 @@ def run_api(ip, port, hawks):
     def tups(self, parts):
       return ((parts[2*n], parts[2*n+1]) for n in range(0, len(parts)/2))
 
+    def ci_dict_get(self, dictionary, key):
+      if key in dictionary:
+        return dictionary.get(key)
+      for d_key in dictionary:
+        if key.lower() == d_key.lower():
+          return dictionary.get(d_key)
+      return None
+
+    def api_set(self, parts):
+      for key,value in parts.iteritems():
+        _val = hawks.settings.get(key)
+        if _val is not None:
+          if type(_val) is float:
+            value = float(value)
+          elif type(_val) is int:
+            value = int(value)
+          self.hawks.settings.set(key, value)
+        else:
+          return self.send(404, body="Unknown attribute: {0}".format(key))
+      hawks.draw_text()
+      return self.send(200)
+
     def do_GET(self):
       parts = map(str.lower, self.path.strip('/').split('/'))
       if not parts or len(parts) % 2 != 0:
@@ -32,20 +54,31 @@ def run_api(ip, port, hawks):
         return self.send(404, body="Unrecognized path: self.path")
 
       if action == 'set':
-        for key,value in self.tups(parts[2:]):
-          _val = hawks.settings.get(key)
-          if _val is not None:
-            if type(_val) is float:
-              value = float(value)
-            elif type(_val) is int:
-              value = int(value)
-            self.hawks.settings.set(key, value)
-          else:
-            return self.send(404, body="Unknown attribute: {0}".format(key))
-        hawks.draw_text()
-        return self.send(200)
+        settings = dict(self.tups(parts[2:]))
+        return self.api_set(settings)
       elif action == 'get':
         return self.send(200, body=json.dumps(hawks.settings.__dict__))
+
+    def do_POST(self):
+      parts = map(str.lower, self.path.strip('/').split('/'))
+
+      if not parts or len(parts) != 2:
+        return self.send(404, body="Path {0} not found".format(self.path))
+
+      cl = self.ci_dict_get(self.headers, 'Content-Length')
+      if not cl:
+        return self.send(400, body="POST body required")
+      body = self.rfile.read(int(cl))
+
+      try:
+        settings = json.loads(body)
+      except Exception as e:
+        return self.send(400, body="Unable to decode POST body:  {0}".format(e))
+
+      if type(settings) != dict:
+        return self.send(400, body="JSON body must contain only a map of key, value pairs")
+
+      return self.api_set(settings)
 
   httpd = BaseHTTPServer.HTTPServer((ip, port), HawksRequestHandler)
   httpd.serve_forever()
